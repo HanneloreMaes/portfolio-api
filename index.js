f00c9896644fdc4364ea926a006cfebabd13ede0
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-10-11 16:10:26 +0200

    no cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const collection = process.env.COLLECTION;
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
