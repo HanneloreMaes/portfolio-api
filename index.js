c75dddcb3e07cd0765c88c717fa80276f98fca9e
HanneloreMaes <hannelore.maes@student.ehb.be>
2022-11-21 15:58:19 +0100

    index.js: routing for category

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ app.get("/", (req, res) => {
  * GET-REQUEST ALL My WORKS FROM API
  * 
  * @param client - getting connection with db as client
- * @returns all books from api
+ * @returns all projects from api
  * 
  */
-app.get("/works", async (req, res) => {
+app.get("/projects", async (req, res) => {
 
 
     try{
@@ -46,12 +46,100 @@ app.get("/works", async (req, res) => {
         await client.connect();
 
         //DATA FROM COLLECTION BOOKAPI  
-        const colli = client.db(dbName).collection("works");
-        const books = await colli.find({}).toArray();
+        const colli = client.db(dbName).collection("Projects");
+        const projects = await colli.find({}).toArray();
 
 
         //GETTIN JSON FILE BACK
-        res.status(200).send(books);
+        res.status(200).send(projects);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            err: "File doesn't excist. Come back later",
+            value: error
+        });
+    }
+    //CLOSING CONNECTION WITH DB
+    finally{
+        await client.close();
+    }
+
+});
+
+/**
+ * GET-REQUEST ALL My Expert Labs FROM API
+ * 
+ * @param client - getting connection with db as client
+ * @returns all expert lab from api
+ * 
+ */
+app.get("/projects/expertLab", async (req, res) => {
+
+
+    try{
+        //CONNECTION MONGDB
+        await client.connect();
+
+        //DATA FROM COLLECTION BOOKAPI  
+        const colli = client.db(dbName).collection("Projects");
+        const query = { category: "expertLab"}
+        const project = await colli.find(query).toArray();
+
+
+
+        //GETTIN JSON FILE BACK
+        if(project){
+            //GETTIN JSON FILE BACK
+            res.status(200).send(project);
+            return
+        }else{
+            res.status(500).send("Project with category 'expertLab' is not found ");
+        }
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            err: "File doesn't excist. Come back later",
+            value: error
+        });
+    }
+    //CLOSING CONNECTION WITH DB
+    finally{
+        await client.close();
+    }
+
+});
+
+/**
+ * GET-REQUEST ALL My Other Projects FROM API
+ * 
+ * @param client - getting connection with db as client
+ * @returns all expert lab from api
+ * 
+ */
+app.get("/projects/otherProjects", async (req, res) => {
+
+
+    try{
+        //CONNECTION MONGDB
+        await client.connect();
+
+        //DATA FROM COLLECTION BOOKAPI  
+        const colli = client.db(dbName).collection("Projects");
+        const query = { category: "otherProject"}
+        const project = await colli.find(query).toArray();
+
+
+
+        //GETTIN JSON FILE BACK
+        if(project){
+            //GETTIN JSON FILE BACK
+            res.status(200).send(project);
+            return
+        }else{
+            res.status(500).send("Project with category 'otherProject' is not found ");
+        }
 
     }catch(error){
         console.log(error);
