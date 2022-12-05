d27e3d6d5e22c6e01b37b154a2f87d198593f8a2
Hannelore Maes <71761907+HanneloreMaes@users.noreply.github.com>
2022-12-05 10:02:57 +0100

    Enable Cors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs/promises');
 const bodyParser = require('body-parser');
 const { MongoClient } = require ("mongodb");
 require('dotenv').config();
-//const cors = require('cors');
+const cors = require('cors');
 
 //CLIENT MONGODB
 const client = new MongoClient(process.env.URL, {
@@ -16,10 +16,9 @@ const collection = process.env.COLLECTION;
 const app = express();
 const port = process.env.PORT;
 
-//app.use(cors());
+app.use(cors());
 app.use(express.static('public'));
 app.use(bodyParser.json());
-//app.use(cors())
 
 //ROOT ROUTE
 app.get("/", (req, res) => {
